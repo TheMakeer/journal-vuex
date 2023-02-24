2aac51f1f6d9ee98fae017fd122a93bea06087c4
TheMakeer <sipkicks@gmail.com>
2023-02-24 10:36:08 -0600

    Initial commit

---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,13 @@
+// export const myMutation = (state) => {};
+
+export const setEntries = (/* state */) => {
+  //
+};
+
+export const createEntry = (/* state */) => {
+  //
+};
+
+export const updateEntry = (/* state */) => {
+  //
+};
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Irure excepteur tempor cillum exercitation. Eiusmod pariatur aliquip voluptate tempor cillum ex occaecat duis in voluptate consequat officia. Nisi nostrud pariatur amet ex ipsum consectetur minim exercitation exercitation anim consequat exercitation. Tempor voluptate nulla dolore cupidatat. Anim excepteur minim consequat enim esse non ea enim anim laboris sunt pariatur minim. Eu culpa nisi duis exercitation voluptate in ad irure est.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "Minim laborum pariatur proident tempor fugiat ullamco. Est in ad quis velit minim laboris mollit ullamco cupidatat velit. Non nulla nulla magna ea nulla est elit consectetur ullamco sit anim. Consequat dolor do sunt consequat nostrud minim nisi sint. Dolore excepteur ipsum veniam sint non est exercitation dolor nostrud deserunt cillum sit id voluptate. Officia quis amet incididunt laboris sint. Eu Lorem consectetur eiusmod cupidatat proident voluptate aute aute ex consequat dolore nostrud laboris aute.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: "Ea commodo ut est sunt esse elit dolore quis pariatur Lorem consectetur officia. Culpa ea mollit laboris dolor cupidatat aute labore sit irure. In mollit ut culpa do tempor. Aliqua aliquip do laborum duis ea labore veniam do tempor aliquip esse anim minim ipsum. Nisi laboris est id sint reprehenderit magna voluptate in ad. Voluptate quis officia sunt nisi et quis sint aliqua laborum eu consequat qui ut do. Ad pariatur laboris fugiat et incididunt incididunt labore non cillum.",
+      picture: null,
+    },
+  ],
+});
